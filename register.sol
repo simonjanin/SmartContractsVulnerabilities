253c34fd1da301f47a40ee932b5104696fbdb45f
Simon Janin <symeof@gmail.com>
2016-06-27 16:35:06 +0200

    Added is_registered function

--- register.sol
+++ register.sol
@@ -2,10 +2,16 @@ contract register {
     mapping(bytes32 => address) domains; // we map hashes to addresses
     
     function register_name(string name) {
-        if (domains[sha3(name)] != 0)
+        if (is_registered(name))
             throw; // domain already registered
         else
             domains[sha3(name)] = msg.sender; // register domain name to msg.sender's name
     }
-    
+	
+	function is_registered(string name) {
+		if (domains[sha3(name)] != 0)
+			return true;
+		else
+			return false;
+	}
 }
